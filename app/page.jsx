7fa83e669f63d649f8ccb555482d872e007f9e4b
hot321ps-jpg <hot321ps@gmail.com>
2026-02-22 00:24:40 +0800

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// 引入您的儀表板組件 (請確認 WarRoomDashboard.jsx 和 page.jsx 放在同一個資料夾，或是根據您的路徑調整)
+import WarRoomDashboard from './WarRoomDashboard';
+
+export default function Home() {
+  return (
+    <main>
+      <WarRoomDashboard />
+    </main>
+  );
+}
