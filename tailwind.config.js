1f0f6dbee41bae4379cec157891077b9a694e28f
hot321ps-jpg <hot321ps@gmail.com>
2026-02-22 00:27:24 +0800

    Update Tailwind CSS content paths for scanning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 新增這行：掃描 src 資料夾底下的所有子資料夾與組件
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", // 如果您有建這個資料夾的話
+    "./*.{js,ts,jsx,tsx}" 
   ],
   theme: {
     extend: {},
